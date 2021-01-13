65202e69df01e4055c9c4a1a7959c903d6619a62
Tina <Tina@macusers-MBP-4.attlocal.net>
2021-01-13 15:22:42 -0600

    public folder created, app.js, index.html, and .css created and linked

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+class App extends React.Component {
+  render = () => {
+    return (
+      <div>
+          <h1>Bird Sightings</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App></App>, document.querySelector('main'))
